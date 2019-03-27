1381414dc08238870bb1aaaca32afae58622abe9
NickoKlevanski <nikolai.klevanski@gmail.com>
2019-03-27 23:52:42 +0200

    html-css-popup: small work

--- docs/app.b2059ea4.js
+++ docs/app.b2059ea4.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"../../node_modules/normalize.css/normalize.css":"DOAq"}],"A2T1":[function(require,module,exports) {
 "use strict";require("./style/main.scss");
 },{"./style/main.scss":"IrIc"}]},{},["A2T1"], null)
-//# sourceMappingURL=app.c7f00e41.js.map
+//# sourceMappingURL=app.b2059ea4.js.map
